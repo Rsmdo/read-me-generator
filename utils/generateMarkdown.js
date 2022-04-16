c62a9a32a3f2a2445449b983fe724214a2ed7d4b
Royden Semedo <roydensemedo@Roydens-MacBook-Pro.local>
2022-04-16 11:37:00 -0400

    added license links

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-    if (data === 'MIT License') {
+    if (license === 'MIT License') {
 
         return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
     
-    }else if (data === 'GNU General Public License v3.0') {
+    }else if (license === 'GNU General Public License v3.0') {
 
         return `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`
     
-    }else if (data === 'Apache 2.0') {
+    }else if (license === 'Apache 2.0') {
         return `![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`
     
-    }else if (data === 'None') {
+    }else if (license === 'None') {
         // empty string 
         return ""; 
     }
@@ -22,7 +22,24 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    if (license === 'MIT License') {
+
+        return `https://choosealicense.com/licenses/mit/`
+    
+    }else if (license=== 'GNU General Public License v3.0') {
+
+        return `https://choosealicense.com/licenses/gpl-3.0/`
+    
+    }else if (license === 'Apache 2.0') {
+        return `https://choosealicense.com/licenses/apache-2.0/`
+    
+    }else if (license === 'None') {
+        // empty string 
+        return ""; 
+    }
+        
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
