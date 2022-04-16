d1468988c684f5771618dc17912001f8c988b2db
Royden Semedo <roydensemedo@Roydens-MacBook-Pro.local>
2022-04-16 11:43:21 -0400

    added renderLicenseLink section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
+    console.log(license);
     if (license === 'MIT License') {
 
         return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
@@ -23,6 +24,7 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
+    console.log(license);
     if (license === 'MIT License') {
 
         return `https://choosealicense.com/licenses/mit/`
@@ -43,7 +45,24 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    console.log(license);
+    if (license === 'MIT License') {
+
+        return `The license used is ${renderLicenseLink(license)}`
+    
+    }else if (license=== 'GNU General Public License v3.0') {
+
+        return `The license used is ${renderLicenseLink(license)}`
+    
+    }else if (license === 'Apache 2.0') {
+        return `The license used is ${renderLicenseLink(license)}`
+    
+    }else if (license === 'None') {
+        // empty string 
+        return ""; 
+    }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
